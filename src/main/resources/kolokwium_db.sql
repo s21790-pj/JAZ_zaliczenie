1e10f131e8ed105fd7d685271cdc44eb61dafff2
s21790-pj <56189587+s21790-pj@users.noreply.github.com>
2021-07-03 14:28:14 +0200

    add sql file

---
+++ src/main/resources/kolokwium_db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS tmep_jaz;
+USE tmep_jaz;
+
+DROP TABLE IF EXISTS currency;
+CREATE TABLE currency
+(
+    id                BIGINT      NOT NULL AUTO_INCREMENT,
+    cur_name          VARCHAR(10) NOT NULL,
+    start_search_date DATE        NOT NULL,
+    end_search_date   DATE        NOT NULL,
+    avg_currency      FLOAT       NOT NULL,
+    search_date       DATE        NOT NULL,
+    search_time       TIME        NOT NULL,
+    PRIMARY KEY (id)
+);
