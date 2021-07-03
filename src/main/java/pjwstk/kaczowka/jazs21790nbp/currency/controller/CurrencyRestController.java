20d07c8fb850f23e84563ba630ae49cbd6b4a8ad
s21790-pj <56189587+s21790-pj@users.noreply.github.com>
2021-07-03 14:37:22 +0200

    Code debug

--- src/main/java/pjwstk/kaczowka/jazs21790nbp/currency/controller/CurrencyRestController.java
+++ src/main/java/pjwstk/kaczowka/jazs21790nbp/currency/controller/CurrencyRestController.java
@@ -1,10 +1,11 @@
 package pjwstk.kaczowka.jazs21790nbp.currency.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pjwstk.kaczowka.jazs21790nbp.currency.service.CurrencyService;
-
-import java.time.LocalDate;
 
 @RestController
 @RequestMapping("")
@@ -16,10 +17,15 @@ public class CurrencyRestController {
         this.currencyService = currencyService;
     }
 
+//    @GetMapping("/temp/{cur_name}")
+//    public ResponseEntity<String> getCurrency(@PathVariable String cur_name,
+//                                              @RequestParam LocalDate start_search_date,
+//                                              @RequestParam LocalDate end_search_date){
+//        return ResponseEntity.ok(currencyService.getCurrency(cur_name, start_search_date, end_search_date));
+//    }
+
     @GetMapping("/temp/{cur_name}")
-    public ResponseEntity<String> getCurrency(@PathVariable String cur_name,
-                                            @RequestParam LocalDate start_search_date,
-                                            @RequestParam LocalDate end_search_date){
-        return ResponseEntity.ok(currencyService.getCurrency(cur_name, start_search_date, end_search_date));
+    public ResponseEntity<Double> getCurrency(@PathVariable String cur_name){
+        return ResponseEntity.ok(currencyService.getCurrency(cur_name));
     }
 }
--- src/main/java/pjwstk/kaczowka/jazs21790nbp/currency/service/CurrencyService.java
+++ src/main/java/pjwstk/kaczowka/jazs21790nbp/currency/service/CurrencyService.java
@@ -27,15 +27,35 @@ public class CurrencyService {
         return curRepository.save(theCurrency);
     }
 
-    public String getCurrency(String cur_name, LocalDate start_search_date, LocalDate end_search_date){
-//        String fooResourceUrl = String.format("http://api.nbp.pl/api/exchangerates/rates/a/%s/%tF/%tF/?format=json", cur_name, start_search_date, end_search_date);
-        String fooResourceUrl = "http://api.nbp.pl/api/exchangerates/rates/a/gbp/2021-02-01/2021-02-07/?format=json";
+//    public String getCurrency(String cur_name, LocalDate start_search_date, LocalDate end_search_date){
+////        String fooResourceUrl = String.format("http://api.nbp.pl/api/exchangerates/rates/a/%s/%tF/%tF/?format=json", cur_name, start_search_date, end_search_date);
+//        String fooResourceUrl = "http://api.nbp.pl/api/exchangerates/rates/a/gbp/2021-02-01/2021-02-07/?format=json";
+//        Root temp_currency = restTemplate.getForEntity(fooResourceUrl, Root.class).getBody();
+//        double x = temp_currency.getRates().get(1).getMid();
+//        List<Double> currency_values = new ArrayList<Double>();
+//        double sum_currency = 0;
+//
+//        return fooResourceUrl;
+//
+//    }
+
+    public Double getCurrency(String cur_name){
+        String fooResourceUrl = String.format("http://api.nbp.pl/api/exchangerates/rates/a/%s/2021-02-01/2021-02-07/?format=json", cur_name);
         Root temp_currency = restTemplate.getForEntity(fooResourceUrl, Root.class).getBody();
-        double x = temp_currency.getRates().get(1).getMid();
+        int days = temp_currency.getRates().size();
         List<Double> currency_values = new ArrayList<Double>();
         double sum_currency = 0;
 
-        return fooResourceUrl;
+        for(int i=0; i<days; i++){
+            currency_values.add(temp_currency.getRates().get(i).getMid());
+            sum_currency += currency_values.get(i);
+        }
+
+        double avg_currency = sum_currency/days;
+//        Currency tempCurrency = new Currency(cur_name, start_search_date, end_search_date, avg_currency);
+//        curRepository.save(tempCurrency);
+
+        return avg_currency;
 
     }
 
